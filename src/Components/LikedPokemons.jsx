eb738f055fba6c71af11642c8389e31c591f6423
Shivangi Sharma <shanussh@gmail.com>
2024-05-29 12:14:24 +0530

    liked pokemon card

--- src/Components/LikedPokemons.jsx
+++ src/Components/LikedPokemons.jsx
@@ -1,20 +1,21 @@
 import React from "react";
+import "./LikedPokemons.css"; // Import the CSS file
 
 const LikedPokemons = ({ likedPokemons }) => {
   return (
     <div className="liked-pokemons-page">
       <h2>Liked Pokémon</h2>
-      <ul>
+      <div className="liked-pokemon-container">
         {likedPokemons.map((pokemon, index) => (
-          <li key={index}>
+          <div className="liked-pokemon-card" key={index}>
             <h3>{pokemon.name}</h3>
             <img
               src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`}
               alt={pokemon.name}
             />
-          </li>
+          </div>
         ))}
-      </ul>
+      </div>
       <button onClick={() => window.history.back()}>Go Back</button>
     </div>
   );
